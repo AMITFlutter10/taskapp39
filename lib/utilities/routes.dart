0f9948c8da6d0edc7065d563d4a11173dc84561e
Eng.Esra Ramadan <97591051+Esraa-amit@users.noreply.github.com>
2023-02-11 16:51:57 +0200

    update

--- lib/utilities/routes.dart
+++ lib/utilities/routes.dart
@@ -7,20 +7,6 @@ class AppRoutes{
   static const String thirdRegisterPageRoute="ThirdRegister";
 
   static const String homePagePageRoute="HomePage";
-  static const String accountSettingPageRoute="AccountSetting";
-  static const String aboutPageRoute="About";
-
-  static const String contactPageRoute="ContactUs";
-  // static const String bottomBarRoute="BottomBar";
-  static const String eventBarRoute="EventPage";
-  // static const String eventPageRoute="EventPage";
-  static const String bottomPageRoute="BottomNav";
-  static const String chatPageRoute="ChatPage";
-  static const String homeDrawerPageRoute="HomeDrawer";
-  static const String profilePageRoute="ProfilePage";
-  static const String channelPageRoute="ChannelPage";
-  static const String channelDetailsPageRoute="ChannelDetails";
-
 
 
 
